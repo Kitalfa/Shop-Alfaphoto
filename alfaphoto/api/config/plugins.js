7b74c8d6fdc40b76722a1f152b7af668e0483f65
Kitalfa <dorian.travers@yahoo.fr>
2024-05-21 10:55:10 +0200

    corrections .env

--- alfaphoto/api/config/plugins.js
+++ alfaphoto/api/config/plugins.js
@@ -1,11 +1,5 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
+  "netlify-deployments": {
     enabled: true,
-    config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      roles: ["strapi-super-admin"],
-    },
   },
 });
